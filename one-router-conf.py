b01243da35b77a37eca7923b9c1c902eff530c8f
Shavindu Senadheera <42552426+shavinsenadheera@users.noreply.github.com>
2020-08-10 18:48:32 +0530

    version 0.1 - netmiko

---
+++ one-router-conf.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+rios = {'device_type':'cisco_ios', 'ip':'192.168.122.211', 'username':'shavindu', 'password':'shavindu'}
+conn = ConnectHandler(**rios)
+
+for i in range(0, 11):
+    print("creating int loop " + str(i))
+    configs = ['int loop ' + str(i), 'ip address ' + str(i+1) + '.' + str(i+1) + '.' + str(i) + '.' + str(i), 'exit']
+    conn.send_config_set(configs)
+saveconf = ['end', 'wr']
+conn.send_config_set(saveconf)
+
+output = conn.send_command('shoeip int br')
+print(output)
